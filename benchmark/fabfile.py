437941b487e19690ceb1551ac9d1ff058f8c8a79
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-08-28 00:59:58 +0200

    added 50

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -96,7 +96,7 @@ def remote(ctx, debug=False):
 
     ''' Run benchmarks on AWS '''
 
-    file_path = 'csv/peformance_data_10.csv'
+    file_path = 'csv/peformance_data_50.csv'
 
     # Open the CSV file
     with open(file_path, mode='r') as file:
