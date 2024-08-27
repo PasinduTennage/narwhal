8ad6bb3d6eb31ebad3731635e2d552d4cd08da2a
“Pasindu <pasindunivanthakatennage@gmail.com>
2024-08-27 21:39:53 +0200

    added and checked summayr.py -- all benchamrks done!

--- benchmark/benchmark/summary.py
+++ benchmark/benchmark/summary.py
@@ -16,9 +16,9 @@ def extract_end_to_end_metrics(file_name: str):
 
         for line in lines:
             if "End-to-end TPS" in line:
-                end_to_end_tps = float(line.split(":")[1].split()[0])
+                end_to_end_tps = float(line.split(":")[1].split()[0].replace(',', ''))
             if "End-to-end latency" in line:
-                end_to_end_latency = float(line.split(":")[1].split()[0])
+                end_to_end_latency = float(line.split(":")[1].split()[0].replace(',', ''))
 
     return [end_to_end_tps, end_to_end_latency]
 
@@ -28,7 +28,7 @@ output = 'csv/summary.csv'
 
 with open(file_path, mode='r') as file:
     csv_reader = csv.reader(file)
-    lines = [['Faults', 'Nodes', 'Rate', 'Size', 'Is_Attack', 'Attack_Level', 'TPS', 'Latency']]
+    lines = [['Faults', 'Nodes', 'Rate', 'Size', 'Is_Attack', 'Attack_Level', 'TPS', 'Latency ms']]
     for index, row in enumerate(csv_reader):
         # Remove any extra spaces and assign to variables
         faults = int(row[0].strip())
@@ -36,17 +36,17 @@ with open(file_path, mode='r') as file:
         is_attack = row[2].strip().lower() == 'true'
         attack_level = int(row[3].strip())
 
-        file = f'benchmark/results/bench-{faults}-{args.nodes}-{1}-{True}-{rate}-{args.size}-{is_attack}-{attack_level}.txt'
+        file = f'results/bench-{faults}-{args.nodes}-{1}-{True}-{rate}-{args.size}-{is_attack}-{attack_level}.txt'
         metrics = extract_end_to_end_metrics(file)
-        metrics.insert(0, faults)
-        metrics.insert(0, args.nodes)
-        metrics.insert(0, rate)
-        metrics.insert(0, args.size)
-        metrics.insert(0, is_attack)
-        metrics.insert(0, attack_level)
+        metrics.insert(0, str(attack_level))
+        metrics.insert(0, str(is_attack))
+        metrics.insert(0, str(args.size))
+        metrics.insert(0, str(rate))
+        metrics.insert(0, str(args.nodes))
+        metrics.insert(0, str(faults))
         lines.append(metrics)
 
 with open(output, mode='w') as file:
-    csv_writer = csv.writer(file)
+    csv_writer = csv.writer(file, quoting=csv.QUOTE_NONE, escapechar='\\')
     for line in lines:
         csv_writer.writerow(line)
